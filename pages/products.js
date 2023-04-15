2b7afeed810579cdc4fb0d851cfc37f2d722be2e
smile168 <jasonli110168@gmail.com>
2023-04-15 15:15:07 -0400

    use absolute url for testing

--- pages/products.js
+++ pages/products.js
@@ -16,8 +16,9 @@ export default function Products({ url }) {
 }
 
 export async function getStaticProps() {
-  const response = await fetch('http://localhost:3000/api/products');
-  let url = await response.json();
+  // const response = await fetch('pages/api/products');
+  let url =
+    'https://xiaopaofu.s3.us-east-1.amazonaws.com/venmo.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIARVH36J24UL4W6EUP%2F20230415%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230415T191442Z&X-Amz-Expires=3600&X-Amz-Signature=b33ede0bc6f810039b07702854bbb8dbc09b9f6238958bf061375122e8cbae23&X-Amz-SignedHeaders=host&x-id=GetObject'; //await response.json();
   return {
     props: {
       url: url,
