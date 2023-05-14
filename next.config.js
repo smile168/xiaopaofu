b2dc926e6295311ecc8c01b12c16311bc967e801
smile168 <jasonli110168@gmail.com>
2023-05-14 01:11:20 -0400

    twist next.config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'xiaopaofu.s3.us-east-1.amazonaws.com',
+//         port: '',
+//       },
+//     ],
+//   },
+// };
+
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'xiaopaofu.s3.us-east-1.amazonaws.com',
-        port: '',
-      },
-    ],
+    domains: ['xiaopaofu.s3.us-east-1.amazonaws.com'],
   },
 };
 
