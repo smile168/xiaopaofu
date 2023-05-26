ec567d5b1f924591f0cedbdd9b64fbdd51b983f4
smile168 <jasonli110168@gmail.com>
2023-05-25 23:17:21 -0400

    tmp take out payment js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <Link href='/payments'>Payments</Link>
+      {/* <Link href='/payments'>Payments</Link> */}
       <br />
       <Link href='https://docs.google.com/document/d/1lyYJVPiGbJaueboqTFposz97mx40t3Zgn3Gx_DN5yhI/edit?usp=sharing'>
         Relx
--- pages/payments.js
+++ pages/payments.js
@@ -1,41 +1,41 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-export default function Payments({ paymentsData }) {
-  const [payments, setPayments] = useState([]);
-  useEffect(() => {
-    setPayments(paymentsData);
-  }, [paymentsData]);
-  const clickToCopyHandler = (e) => {
-    navigator.clipboard.writeText(e.target.value);
-  };
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <th scope='col'>Category</th>
-          <th scope='col'>SearchBy</th>
-          <th scope='col'>registerAs</th>
-        </tr>
-        {payments.map((payment) => (
-          <tr key={payment.id}>
-            <td>{payment.category}</td>
-            <td>
-              <button value={payment.searchBy} onClick={clickToCopyHandler}>
-                Copy
-              </button>
-            </td>
-            <td>{payment.registerAs}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+// export default function Payments({ paymentsData }) {
+//   const [payments, setPayments] = useState([]);
+//   useEffect(() => {
+//     setPayments(paymentsData);
+//   }, [paymentsData]);
+//   const clickToCopyHandler = (e) => {
+//     navigator.clipboard.writeText(e.target.value);
+//   };
+//   return (
+//     <table>
+//       <tbody>
+//         <tr>
+//           <th scope='col'>Category</th>
+//           <th scope='col'>SearchBy</th>
+//           <th scope='col'>registerAs</th>
+//         </tr>
+//         {payments.map((payment) => (
+//           <tr key={payment.id}>
+//             <td>{payment.category}</td>
+//             <td>
+//               <button value={payment.searchBy} onClick={clickToCopyHandler}>
+//                 Copy
+//               </button>
+//             </td>
+//             <td>{payment.registerAs}</td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// }
 
-export async function getStaticProps() {
-  const response = await fetch(`${process.env.SERVER}/api/payments`);
-  let payments = await response.json();
-  return {
-    props: { paymentsData: payments },
-  };
-}
+// export async function getStaticProps() {
+//   const response = await fetch(`${process.env.SERVER}/api/payments`);
+//   let payments = await response.json();
+//   return {
+//     props: { paymentsData: payments },
+//   };
+// }
