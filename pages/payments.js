c7e184e1c784c48fda62ea9a505146511d822d76
smile168 <jasonli110168@gmail.com>
2023-06-09 01:02:57 -0400

    payments copy css

--- pages/payments.js
+++ pages/payments.js
@@ -36,7 +36,7 @@ export default function Payments({ paymentsData }) {
                 <button
                   value={payment.searchBy}
                   onClick={clickToCopyHandler}
-                  class='px-4 py-1 text-sm font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'
+                  class='px-4 py-1 text-sm font-semibold rounded-full border  hover:text-white hover:bg-slate-400 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2'
                 >
                   Copy
                 </button>
