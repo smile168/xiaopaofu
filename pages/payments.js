2bdf527f193a446151c622149ebb793e7c95eede
smile168 <jasonli110168@gmail.com>
2023-06-09 01:01:06 -0400

    payments css

--- pages/payments.js
+++ pages/payments.js
@@ -9,38 +9,43 @@ export default function Payments({ paymentsData }) {
     navigator.clipboard.writeText(e.target.value);
   };
   return (
-    <table class='m-auto w-1/2 h-5 text-center '>
-      <thead>
-        <tr class='table-row'>
-          <th scope='col' class='table-cell'>
-            Category
-          </th>
-          <th scope='col' class='table-cell'>
-            RegisterAs
-          </th>
-          <th scope='col' class='table-cell'>
-            SearchBy
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {payments.map((payment) => (
-          <tr key={payment.id} class='table-row'>
-            <td class='table-cell'> {payment.category}</td>
-            <td class='table-cell'>{payment.registerAs}</td>
-            <td class='table-cell'>
-              <button
-                value={payment.searchBy}
-                onClick={clickToCopyHandler}
-                class='px-4 py-1 text-sm font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'
-              >
-                Copy
-              </button>
-            </td>
+    <>
+      <h1 class='m-auto w-4/5 '>
+        请选择以下任意一种支付方式，点击相应行的Copy按钮,然后去相应软件粘贴
+      </h1>
+      <table class='m-auto w-4/5 text-center'>
+        <thead>
+          <tr class='table-row '>
+            <th scope='col' class='table-cell py-3'>
+              Category
+            </th>
+            <th scope='col' class='table-cell py-3'>
+              RegisterAs
+            </th>
+            <th scope='col' class='table-cell py-3'>
+              SearchBy
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {payments.map((payment) => (
+            <tr key={payment.id} class='table-row border-y-2'>
+              <td class='table-cell py-3'> {payment.category}</td>
+              <td class='table-cell py-3'>{payment.registerAs}</td>
+              <td class='table-cell py-3'>
+                <button
+                  value={payment.searchBy}
+                  onClick={clickToCopyHandler}
+                  class='px-4 py-1 text-sm font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'
+                >
+                  Copy
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
