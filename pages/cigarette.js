f30fc3c8d859d5e427967d5733cfb73602154b2a
smile168 <jasonli110168@gmail.com>
2023-07-07 18:01:15 -0400

    take out Cigarettes impl

--- pages/cigarette.js
+++ pages/cigarette.js
@@ -3,13 +3,14 @@ export default function Cigarette({ cigarette, onSelectCigarette }) {
     onSelectCigarette(cigarette._id);
   };
   return (
-    <tr key={cigarette._id} onClick={selctCigaretteHandler}>
-      <td className='border-t-2 border-slate-900 py-2.5'>{cigarette.brand}</td>
-      <td className='border-t-2 border-slate-900 py-2.5'>{cigarette.name}</td>
-      <td className='border-t-2 border-slate-900 py-2.5'>
-        {cigarette.quantity}
-      </td>
-      <td className='border-t-2 border-slate-900 py-2.5'>{cigarette.price}</td>
-    </tr>
+    // <tr key={cigarette._id} onClick={selctCigaretteHandler}>
+    //   <td className='border-t-2 border-slate-900 py-2.5'>{cigarette.brand}</td>
+    //   <td className='border-t-2 border-slate-900 py-2.5'>{cigarette.name}</td>
+    //   <td className='border-t-2 border-slate-900 py-2.5'>
+    //     {cigarette.quantity}
+    //   </td>
+    //   <td className='border-t-2 border-slate-900 py-2.5'>{cigarette.price}</td>
+    // </tr>
+    <></>
   );
 }
