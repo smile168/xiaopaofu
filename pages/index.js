f005e0823c51adb1a54f976d0c2d4cf0c13cd8a8
smile168 <jasonli110168@gmail.com>
2023-09-28 23:50:42 -0400

    pot change

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,12 @@
-import Link from 'next/link';
-import zgarLogo from '../public/zgar-logo.png';
-import Image from 'next/image';
-import vercel from '../public/vercel.svg';
-import relxLogo from '../public/relx-logo.png';
-import payLogo from '../public/pay-logo.png';
+import Home from './home';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-export default function Home() {
+const queryClient = new QueryClient();
+
+export default function Index() {
   return (
-    <div class='flex:none bg-gray-100 sm:flex '>
-      <div class='flex-1 rounded-2xl overflow-hidden shadow-lg m-8'>
-        <Link href='/zgar'>
-          <Image
-            class='object-fill h-full w-full'
-            src={zgarLogo}
-            alt={vercel}
-          />
-        </Link>
-      </div>
-      <div class='flex-1 rounded-2xl overflow-hidden shadow-lg  m-8'>
-        <Link href='/payments' class='h-full w-full'>
-          <Image class='object-fill h-full w-full' src={payLogo} alt={vercel} />
-        </Link>
-      </div>
-      <div class='flex-1 rounded-2xl overflow-hidden shadow-lg  m-8'>
-        <Link href='https://docs.google.com/document/d/1lyYJVPiGbJaueboqTFposz97mx40t3Zgn3Gx_DN5yhI/edit?usp=sharing'>
-          <Image
-            class='object-fill h-full w-full'
-            src={relxLogo}
-            alt={vercel}
-          />
-        </Link>
-      </div>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   );
 }
