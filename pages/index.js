ca0d64a26023dfec63725dc36580b5b9f7b947cf
smile168 <jasonli110168@gmail.com>
2023-09-29 00:22:08 -0400

    clean up query client

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 import Home from './home';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient();
 
 export default function Index() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Home />
-    </QueryClientProvider>
-  );
+  return <Home />;
 }
